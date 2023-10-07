127f3ed61e5500872ecb151b2553f52aa929725c
Roy Ayala <troysg8203@gmail.com>
2023-10-07 15:36:12 -0400

    round planet

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import './planet.css'
 
 import App from './App.jsx'
 import NavBar from './pages/NavBar.jsx'
