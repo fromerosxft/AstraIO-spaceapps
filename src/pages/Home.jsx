743cf98e9d0f5e9db8d9f8d2333b6bd8d1fd5918
Roy Ayala <troysg8203@gmail.com>
2023-10-08 08:11:11 -0400

    add git ignore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,41 +4,6 @@ import React from "react";
 function Home() {
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> daee37b (fix problems images)
-    <section>
-    <div className="carousel w-full bg-gray-900">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img
-            src="./src/imgs/planeta77.jpg"
-            className="w-full"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-<<<<<<< HEAD
-          </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img
-            src="./src/imgs/planeta11.jpeg"
-            className="w-full"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img
-            src="./src/imgs/planeta66.jpg"
-            className="w-full"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          </div>
-        </div>
-      </div>
-    </section>
-      
-=======
       <section>
         <div className="carousel w-full bg-gray-900">
           <div id="slide1" className="carousel-item relative w-full">
@@ -59,35 +24,8 @@ function Home() {
       <GetStart />
       <Consider />
       <Sections />
->>>>>>> 66eb959 (add game)
-=======
-          </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img
-            src="./src/imgs/planeta11.jpeg"
-            className="w-full"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img
-            src="./src/imgs/planeta66.jpg"
-            className="w-full"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          </div>
-        </div>
-      </div>
-    </section>
-      
->>>>>>> daee37b (fix problems images)
     </>
   );
 }
-
-
-
 
 export default Home;
