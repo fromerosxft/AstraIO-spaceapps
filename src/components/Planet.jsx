4a8cfe73d5e1127603cf14517b40a12f2153b4dd
Roy Ayala <troysg8203@gmail.com>
2023-10-08 03:38:51 -0400

    work litter plannet

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Planet() {
   return (
-    <figure className='w-40 h-40 bg-transparent relative z-0'>
-      <img className='h-40 w-full object-cover rounded-full absolute z-1' src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/332937/earth.jpg" alt="planeta photo"/>
-      <div className='h-40 w-40 bg-transparent shadow-plt rounded-full absolute z-6'/>
-    </figure>
+    <div className='w-40 h-40 bg-transparent relative z-0'>
+      <div className="circle animate-moving h-40 w-full absolute z-1 rounded-full bg-repeat-x bg-cover bg-[right_0rem_top_0rem]  bg-[url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/332937/earth.jpg')]"></div>
+      {/* <img className='h-40 w-full object-cover rounded-full absolute z-1 bg-repeat-x bg-[center_top_10rem]' src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/332937/earth.jpg" alt="planeta photo"/> */}
+      <div className='h-40 w-40 bg-transparent shadow-plt rounded-full absolute z-2'/>
+    </div>
   )
 }
 
