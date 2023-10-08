e37b72668e787641f9d8a774de66a67e1a5de394
Roy Ayala <troysg8203@gmail.com>
2023-10-08 05:07:35 -0400

    work but need to spin

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-
-function Planet() {
+function Planet({size, imageUrl}) {
   return (
-    <div className='w-40 h-40 bg-transparent relative z-0'>
-      <div className="circle animate-moving h-40 w-full absolute z-1 rounded-full bg-repeat-x bg-cover bg-[right_0rem_top_0rem]  bg-[url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/332937/earth.jpg')]"></div>
-      {/* <img className='h-40 w-full object-cover rounded-full absolute z-1 bg-repeat-x bg-[center_top_10rem]' src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/332937/earth.jpg" alt="planeta photo"/> */}
-      <div className='h-40 w-40 bg-transparent shadow-plt rounded-full absolute z-2'/>
+    <div className={`h-${size} w-${size} bg-transparent relative z-0`}>
+      <div className={`circle h-full w-full animate-moving absolute z-1 rounded-full bg-repeat-x bg-cover bg-[right_0rem_top_0rem]  bg-[url('${imageUrl}')]`}></div>
+      <div className={`h-${size} w-${size} bg-transparent shadow-plt rounded-full absolute z-2`}/>
     </div>
   )
 }
 
-export default Planet
+export default Planet;
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import Planet from '../components/Planet.jsx'
+import Planet from "../components/Planet.jsx";
 
 function Game() {
   return (
-    <main className='w-full bg-slate-400 flex justify-center'>
-        <div className="container w-2/3 p-4 m-4 border-4 border-neutral rounded-xl h-96 bg-white">
-            <Planet/>
+    <main className="w-full bg-white flex justify-center">
+      <div className="container w-2/3 p-4 m-4 border-4 border-white rounded-xl h-96 bg-black flex items-center justify-center ">
+        <div>
+          <Planet
+            size="20"
+            imageUrl="https://s3-us-west-2.amazonaws.com/s.cdpn.io/332937/earth.jpg"
+          />
         </div>
+        <div>
+          <Planet
+            size="60"
+            imageUrl="https://s3-us-west-2.amazonaws.com/s.cdpn.io/332937/sun.jpg"
+          />
+        </div>
+      </div>
     </main>
-  )
+  );
 }
 
-export default Game
+export default Game;
